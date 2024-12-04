98ffc7c31501cdacb9da08a91319697fce68dac4
Byron R. Harder <byrontheii@gmail.com>
2024-12-03 21:17:17 -0500

    Day 3 and update to data file naming

--- src/lib.rs
+++ src/lib.rs
@@ -4,19 +4,17 @@ use std::fs;
 use std::error::Error;
 mod day01;
 mod day02;
+mod day03;
 
 fn make_path(args: &Vec<String>) -> String {
-    let ex = match args[3].as_str() {
-        "ex" => "_ex",
-        _ => "",
-    };
-    format!("data/{}{}.txt", args[1], ex)
+    format!("data/{}{}.txt", args[1], if args.len() >= 4 {format!("_{}", args[3])} else {"".to_string()})
 }
 
 pub fn run(args: &Vec<String>) -> Result<String, Box<dyn Error>> {
     let day: Box<dyn Runner> = match args[1].as_str() {
         "01" => Box::new(day01::Day01{file_path: make_path(args)}),
         "02" => Box::new(day02::Day02{file_path: make_path(args)}),
+        "03" => Box::new(day03::Day03{file_path: make_path(args)}),
         _ => Err(format!("Unrecognized command '{}'", args[1]))?
     };
     match args[2].as_str() {
